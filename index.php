3bb0a835c24c872007570f9fca8c6b283863c70b
João Afonso <joaoafonso386@gmail.com>
2022-03-29 23:28:34 +0100

    Add sanitazation to form inputs

--- index.php
+++ index.php
@@ -8,43 +8,56 @@
 
   if( isset($_POST["send"]) ) {
 
-    $mail = new PHPMailer(true);
-
-    try {
-
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                     
-      $mail->isSMTP();                                           
-      $mail->Host       = 'smtp.example.com';                    
-      $mail->SMTPAuth   = true;                                  
-      $mail->Username   = 'user@example.com';                     
-      $mail->Password   = 'secret';                               
-      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
-      $mail->Port       = 465;                                    
-  
-      //Recipients
-      $mail->setFrom('from@example.com', 'Mailer');
-      $mail->addAddress('joe@example.net', 'Joe User');     
-      $mail->addAddress('ellen@example.com');               
-      $mail->addReplyTo('info@example.com', 'Information');
-      $mail->addCC('cc@example.com');
-      $mail->addBCC('bcc@example.com');
-  
-      //Attachments
-      $mail->addAttachment('/var/tmp/file.tar.gz');         
-      $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
-  
-      //Content
-      $mail->isHTML(true);                                  
-      $mail->Subject = 'Here is the subject';
-      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-  
-      $mail->send();
-      echo 'Message has been sent';
-
-    } catch(Exception $e) {
-      echo $e;
+    foreach($_POST as $key => $form_value) {
+      $_POST[$key] = trim(htmlspecialchars((strip_tags($form_value))));
     }
+
+    if(
+      filter_var($_POST["sender_email"], FILTER_VALIDATE_EMAIL) &&
+      filter_var($_POST["receiver_email"], FILTER_VALIDATE_EMAIL) 
+      ) {
+
+        $mail = new PHPMailer(true);
+
+        try {
+
+          $mail->SMTPDebug = SMTP::DEBUG_SERVER;                     
+          $mail->isSMTP();                                           
+          $mail->Host       = 'smtp.example.com';                    
+          $mail->SMTPAuth   = true;                                  
+          $mail->Username   = 'user@example.com';                     
+          $mail->Password   = 'secret';                               
+          $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+          $mail->Port       = 465;                                    
+      
+          //Recipients
+          $mail->setFrom('from@example.com', 'Mailer');
+          $mail->addAddress('joe@example.net', 'Joe User');     
+          $mail->addAddress('ellen@example.com');               
+          $mail->addReplyTo('info@example.com', 'Information');
+          $mail->addCC('cc@example.com');
+          $mail->addBCC('bcc@example.com');
+      
+          //Attachments
+          $mail->addAttachment('/var/tmp/file.tar.gz');         
+          $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
+      
+          //Content
+          $mail->isHTML(true);                                  
+          $mail->Subject = 'Here is the subject';
+          $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+          $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+      
+          $mail->send();
+          echo 'Message has been sent';
+
+        } catch(Exception $e) {
+          echo $e;
+        }
+
+      }
+
+    
 
   }
 
