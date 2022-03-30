28ee77f3d05950d75498cb3f4bce53f9e93116e3
João Afonso <joaoafonso386@gmail.com>
2022-03-30 23:20:45 +0100

    Config email settings, body and subject

--- index.php
+++ index.php
@@ -35,20 +35,12 @@
           //Recipients
           $mail->setFrom('from@example.com', 'Mailer');
           $mail->addAddress('joe@example.net', 'Joe User');     
-          $mail->addAddress('ellen@example.com');               
-          $mail->addReplyTo('info@example.com', 'Information');
-          $mail->addCC('cc@example.com');
-          $mail->addBCC('bcc@example.com');
-      
-          //Attachments
-          $mail->addAttachment('/var/tmp/file.tar.gz');         
-          $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
       
           //Content
           $mail->isHTML(true);                                  
-          $mail->Subject = 'Here is the subject';
-          $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-          $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+          $mail->Subject = 'Send me money please! Urgent!';
+          $mail->Body    = 'Hey, I really need you to send me {X} amount of money ASAP! It really is for a good cause';
+          $mail->AltBody = 'Hey, I really need you to send me {X} amount of money ASAP! It really is for a good cause';
       
           $mail->send();
           echo 'Message has been sent';
